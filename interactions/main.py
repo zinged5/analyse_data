25094a20facdb6693913b329c6f90a1d307f7c87
aagupt <amritagupta2016@gmail.com>
2023-02-24 11:19:01 +0000

    v1

---
+++ interactions/main.py
@@ -0,0 +1,10 @@
+from os import path
+import pyarrow as pa
+from pyarrow import csv
+import pandas as pd
+from python311 import os
+from dataclasses import dataclass
+
+@dataclass
+class analyse_data():
+    pass
